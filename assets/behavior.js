548a4002fbaf3b9fa36cbe48a1205c275a638d30
aureooms <aurelien.ooms@gmail.com>
2015-04-18 14:08:40 +0200

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -179,6 +179,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           },
           "depth": 2,
           "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-bit/js/src/hack/sign.js",
+            "projectPath": "js/src/hack/sign.js",
+            "targetPath": "hack/sign",
+            "pageTitle": "hack/sign",
+            "title": "sign"
+          },
+          "depth": 2,
+          "outline": []
         }
       ]
     }, {
